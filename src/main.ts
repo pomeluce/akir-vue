c4e5b0dfcc8da94a02a135897ce304ba98f21fad
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2025-01-27 13:37:48 +0800

    update: 更新了入口文件配置

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import App from '@/App.tsx';
 import plugins from '@/plugins';
-// import directives from '@/directives';
-import '@/styles/global.scss';
 
 const bootstrap = (): void => {
   // 创建 vue 实例对象
   const app = createApp(App);
   // 挂载插件
   plugins(app);
-  // 挂载指令
-  // directives(app);
   // 挂载到 app 中
   app.mount('#app');
 };
