917dcf70a1dbf14b310af53419d94102d37c478a
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2025-03-11 09:42:02 +0800

    perf: Optimized useTabs hook function

--- src/hooks/system/useTabs.ts
+++ src/hooks/system/useTabs.ts
@@ -2,38 +2,36 @@ import { defaultTab } from '@/configs/tabs';
 
 const storage = useStorage({ domain: 'session' });
 
+let initialized = false;
+
 export default () => {
   const state: TabState = reactive({
     active: defaultTab,
-    tabs: [],
+    tabs: [defaultTab],
     excludes: [],
   });
 
-  getCurrentScope()?.run(() => {
+  if (!initialized) {
+    initialized = true;
     Object.assign(state, storage.get<TabState>(CacheKey.TABS_ROUTER));
-  });
-
-  function wrapTabRouter<T extends any[]>(fn: (...args: T) => void) {
-    return (...args: T) => {
-      fn(...args);
-      storage.set(CacheKey.TABS_ROUTER, state);
-    };
   }
 
-  const setActiveTab = wrapTabRouter((tab: TabType = defaultTab) => {
-    state.active = tab;
-  });
+  watch(state, value => storage.set(CacheKey.TABS_ROUTER, value), { immediate: true });
 
-  const addTab = wrapTabRouter((tab: TabType) => {
+  const setActiveTab = (tab: TabType = defaultTab) => {
+    state.active = tab;
+  };
+
+  const addTab = (tab: TabType) => {
     const filterTab = state.tabs.find(item => item.key === tab.key);
     if (!filterTab) state.tabs.push(tab);
     state.excludes = state.excludes.filter(key => key !== tab.key);
-  });
+  };
 
-  const removeTab = wrapTabRouter((tab: TabType) => {
+  const removeTab = (tab: TabType) => {
     state.tabs = state.tabs.filter(item => item.key !== tab.key);
     state.excludes.push(tab.key);
-  });
+  };
 
   return { state: readonly(state), setActiveTab, addTab, removeTab };
 };
